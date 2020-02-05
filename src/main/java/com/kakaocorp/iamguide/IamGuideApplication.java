d51284e674f53884d57699a81efa4c53bf56b1b3
pitzza.lee <>
2020-02-05 10:41:33 +0900

    no message

--- src/main/java/com/kakaocorp/iamguide/IamGuideApplication.java
+++ src/main/java/com/kakaocorp/iamguide/IamGuideApplication.java
@@ -2,12 +2,19 @@ package com.kakaocorp.iamguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class IamGuideApplication {
+public class IamGuideApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(IamGuideApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringApplicationBuilder.class);
     }
 }
